84cbcc7f67796dfb1c35e0895fc7129823455b6c
Rodrigo Chumacero <rodrychm@gmail.com>
2019-01-09 10:19:20 -0400

    #16 Se aumento para la parametrizaciones gasto ingreso y utilida

--- vista/cuenta/Cuenta.php
+++ vista/cuenta/Cuenta.php
@@ -477,7 +477,9 @@ Phx.vista.Cuenta=Ext.extend(Phx.arbGridInterfaz,{
                                  ['curso', 'Obras en Curso (Considera Centro de Costo)'],
                                  ['reservas', 'Actualización de reservas'],
                                  ['capital', 'Actualización de capital'],
-                                 ['utilidad', 'Actualización de Utilidad'] //#16
+                                 ['gasto', 'Actualización de Gasto'] ,//#16
+                                 ['utilidad', 'Actualización de Utilidad'], //#16
+                                 ['ingresos', 'Actualización de Ingresos'] //#16
                                ]
                         }),
        				valueField: 'variable',
