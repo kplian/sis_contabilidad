8f3741f0148677909ecf08cc2c07fc3a4e055560
rarteaga <rensi@kplian.com>
2020-01-03 14:31:37 -0400

    #84 convertir moneda en la funcion que armar errores de presupeusto para contabilidad

--- base/funciones/conta.f_armar_error_presupuesto.sql
+++ base/funciones/conta.f_armar_error_presupuesto.sql
@@ -22,8 +22,9 @@ $body$
    	
  ISSUE            FECHA:		      AUTOR                 DESCRIPCION
    
- #0        		29-03-2018        RAC KPLIAN        Mejora los mensajes derror dejandolos mas claros
-#73             19/09/2019            EGS                   Se modifica mensaje de error al aprobar ajuste
+ #0        		29-03-2018          RAC KPLIAN        Mejora los mensajes derror dejandolos mas claros
+#73             19/09/2019          EGS                   Se modifica mensaje de error al aprobar ajuste
+#84             03/01/2020          RAC         Corregir mensaje de error en ajustes presupuestarios para mostrar el monto faltante en la moneda base
 */
 
 DECLARE
@@ -32,6 +33,7 @@ DECLARE
   v_codico_cc varchar;
   v_mensaje_error varchar;
   v_tmp varchar;
+  v_monto_aux_mb numeric; --#26
   
 BEGIN
   v_nombre_funcion = 'conta.f_armar_error_presupuesto';
@@ -51,12 +53,21 @@ BEGIN
                   v_tmp = format('el presupuesto no alcanza por diferencia cambiaria, en moneda base tenemos:   %s y se requiere %s ', round(p_resp_ges[3],2)::varchar,  round(p_monto_cmp_mb,2)::varchar);
              ELSE
              
-             
               
                   IF p_id_moneda_base = p_id_moneda THEN
                       v_tmp = format('solo se tiene disponible un monto en moneda base de:  %s y se requiere; %s (Falta . %s)', round(p_resp_ges[3],2)::varchar, round(p_monto_cmp_mb,2)::varchar,  round(p_resp_ges[3] -abs(p_monto_cmp_mb),2)::varchar);    --#73
                   ELSE
-                      v_tmp =  format('solo se tiene disponible un monto de:  %s y se requiere  %s. (Falta . %s)', round(p_resp_ges[3],2)::varchar, round(p_monto_cmp_mb,2)::varchar,  round(p_resp_ges[3] - p_monto_cmp_mb,2)::varchar);
+                  
+           
+                      --#84
+                      v_monto_aux_mb =   param.f_convertir_moneda (  
+                                                                   p_id_moneda, 
+                                                                   p_id_moneda_base,
+                                                                   p_monto_cmp_mb, 
+                                                                   now()::date,
+                                                                   'O',50); 
+                       
+                      v_tmp =  format('solo se tiene disponible un monto de:  %s y se requiere  %s. (Falta . %s)', round(p_resp_ges[3],2)::varchar, round(v_monto_aux_mb,2)::varchar,  round(p_resp_ges[3] - p_monto_cmp_mb,2)::varchar);
                   END IF;
                                             
              END IF;
