ad4b562d5e970e9a70463fd830c51811590866bb
Administrator <Administrator@endetransmision.bo>
2020-09-27 18:59:32 -0400

    MSA-34 Se modifica inicio y final del archivo para actualizacion de datos en BD

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -1,3 +1,3 @@
-/********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
---SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
 /********************************************I-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
+--SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
+/********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
