06e85b203582b2b7732976576dcc2cbc7522ac7f
mguerra <manuel.guerra@endetransmision.bo>
2020-09-30 16:51:27 -0400

    #ETR-1180 CARGA DE FACTURA CON DATOS ERRONEOS EN CAJA

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -6,13 +6,13 @@
 
 /********************************************I-DAUP-MGM-SCHEMA-0-30/09/2020********************************************/
 
-UPDATE conta.tdoc_compra_venta d
-SET d.importe_doc = 1973.44,d.importe_neto=1776.10,d.importe_pago_liquido=1776.10
-WHERE d.id_doc_compra_venta = 182885
+UPDATE conta.tdoc_compra_venta 
+SET importe_doc = 1973.44,importe_neto=1776.10,importe_pago_liquido=1776.10
+WHERE id_doc_compra_venta = 182885
 
 UPDATE conta.tdoc_concepto 
-SET c.precio_unitario= 1973.44,c.precio_total= 1973.44,c.precio_total_final=1776.10
-WHERE c.id_doc_compra_venta=182885
+SET precio_unitario= 1973.44,precio_total= 1973.44,precio_total_final=1776.10
+WHERE id_doc_compra_venta=182885
 /********************************************F-DAUP-MGM-SCHEMA-0-30/09/2020********************************************/
 
 
