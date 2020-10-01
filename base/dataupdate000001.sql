d3cf52f12a696705444552e58fe55034d6339e59
mguerra <manuel.guerra@endetransmision.bo>
2020-09-30 21:12:38 -0400

    #ETR-779 MODIFICACION DE NOTAS DE DEBITO NO CARGADAS

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -18,6 +18,35 @@ UPDATE conta.tdoc_compra_venta SET importe_doc = 1973.44,importe_neto=1776.10,im
 UPDATE conta.tdoc_concepto SET precio_unitario= 1973.44,precio_total= 1973.44,precio_total_final=1776.10 WHERE id_doc_compra_venta=182885;
 /********************************************F-DAUP-MGM-SCHEMA-0-30/09/2020********************************************/
 
+/********************************************I-DAUP-MGM-CONTA-1-30/09/2020********************************************/
+--rollback
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181038 
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181039
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181040
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181041
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181042
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181982
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181984
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181987
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181988
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181991
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181996
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181997
+--UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='' WHERE id_doc_compra_venta = 181998
 
+--commit
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232572' WHERE id_doc_compra_venta = 181038 
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232641' WHERE id_doc_compra_venta = 181039
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232816' WHERE id_doc_compra_venta = 181040
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232816' WHERE id_doc_compra_venta = 181041
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232811' WHERE id_doc_compra_venta = 181042
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='233020' WHERE id_doc_compra_venta = 181982
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='233022' WHERE id_doc_compra_venta = 181984
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='233023' WHERE id_doc_compra_venta = 181987
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='233026' WHERE id_doc_compra_venta = 181988
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232987' WHERE id_doc_compra_venta = 181991
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='232986' WHERE id_doc_compra_venta = 181996
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='233025' WHERE id_doc_compra_venta = 181997
+UPDATE conta.tdoc_compra_venta SET nota_debito_agencia='233021' WHERE id_doc_compra_venta = 181998
 
-
+/********************************************F-DAUP-MGM-CONTA-1-30/09/2020********************************************/
