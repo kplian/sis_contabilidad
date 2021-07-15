435e6bb270690a6de7f23fd6f933a10ea6fc015c
Jaime Rivera <jriverarojas@gmail.com>
2021-07-15 18:27:25 -0400

    Update patch000001.sql
    remove alter table to concepto_ingas

--- base/patch000001.sql
+++ base/patch000001.sql
@@ -939,30 +939,6 @@ ALTER TABLE conta.ttransaccion
 
 
 
-/***********************************I-SCP-RAC-CONTA-0-11/09/2014****************************************/
-
---------------- SQL ---------------
-
-ALTER TABLE param.tconcepto_ingas
-  ADD COLUMN requiere_ot VARCHAR(20) DEFAULT 'opcional' NOT NULL;
-
-COMMENT ON COLUMN param.tconcepto_ingas.requiere_ot
-IS 'obligatorio, opcional';
-
---------------- SQL ---------------
-
-ALTER TABLE param.tconcepto_ingas
-  ADD COLUMN filtro_ot VARCHAR(25) DEFAULT 'todos' NOT NULL;
-
-COMMENT ON COLUMN param.tconcepto_ingas.filtro_ot
-IS 'todos, listado  , si es listado filtra atravez de la relacion ot_concepto_gasto';
-
-
-/***********************************F-SCP-RAC-CONTA-0-11/09/2014****************************************/
-
-
-
-
 /***********************************I-SCP-RAC-CONTA-0-02/10/2014****************************************/
 
 --------------- SQL ---------------
